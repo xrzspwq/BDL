da8514e895720030f170f4ce96c246074ef0a5a4
xrzspwq <105160260+xrzspwq@users.noreply.github.com>
2025-04-05 21:32:13 +0200

    commentaire de moon

--- window/AppMenuBar.java
+++ window/AppMenuBar.java
@@ -18,29 +18,63 @@ public class AppMenuBar
 
     }
 
+    /**
+     * Constructs and returns a File menu for the application's menu bar.
+     * The File menu contains a list of menu items specified in the {@link MenuBarElem#fileMenuItems} list.
+     *
+     * @return A {@link Menu} object representing the File menu.
+     */
     private Menu consFileMenu()
     {
+        // Create an array of MenuItem objects based on the fileMenuItems list
         MenuItem [] items = new MenuItem[ MenuBarElem.fileMenuItems.size()];
         for(int i=0; i < MenuBarElem.fileMenuItems.size(); ++i )
         {
             items[i] = new MenuItem( MenuBarElem.fileMenuItems.get(i));
         }
+
+        // Return a new Menu object with the label "File", a VBox layout, and the created menu items
         return new Menu("File",new VBox(),items);
-    
     }
 
+
+    /**
+     * Constructs and returns an Edit menu for the application's menu bar.
+     * The Edit menu currently does not contain any specific menu items.
+     *
+     * @return A {@link Menu} object representing the Edit menu.
+     *         The menu has the label "Edit" and is displayed using a {@link VBox} layout.
+     */
     private Menu consEditMenu()
     {
-        return new Menu("Edit",new VBox());
+        return new Menu("Edit", new VBox());
     }
 
+
+    /**
+     * Constructs and returns a View menu for the application's menu bar.
+     * The View menu currently does not contain any specific menu items.
+     *
+     * @return A {@link Menu} object representing the View menu.
+     *         The menu has the label "View" and is displayed using a {@link VBox} layout.
+     */
     private Menu consViewMenu()
     {
-        return new Menu("View",new VBox());
+        return new Menu("View", new VBox());
     }
 
+
+    /**
+     * Returns the menu bar panel for the application.
+     *
+     * @return The {@link javafx.scene.control.MenuBar} object representing the menu bar panel.
+     *         This menu bar contains the File, Edit, and View menus, which are constructed
+     *         using the {@link #consFileMenu()}, {@link #consEditMenu()}, and {@link #consViewMenu()} methods, respectively.
+     *         The menu bar is configured to use the system menu bar if the operating system supports it.
+     */
     public MenuBar getPanel()
     {
         return menuBar;
-    } 
+    }
+
 }
--- window/GraphicElem.java
+++ window/GraphicElem.java
@@ -5,7 +5,6 @@ import src.Elem;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import src.*;
 import javafx.scene.shape.*;
 import java.awt.geom.Point2D;
@@ -14,152 +13,169 @@ import javafx.scene.shape.Circle;
 import javafx.geometry.Bounds;
 import javafx.geometry.BoundingBox;
 
-
-
-public class GraphicElem
-{
-    public enum Orientation
-    {
+public class GraphicElem {
+    public enum Orientation {
         EAST,
         SOUTH,
         WEST,
         NORTH,
     }
-    
+
     private final static double inputRadius = 5;
     private final static double inputMargin = 10;
     private final static double inputSpacing = 4;
-    static int idCount=0;
+    static int idCount = 0;
     private final int id;
     private final Elem elem;
     private List<Object> attributes;
     private Shape shape;
     private List<Circle> inputs;
     private List<Circle> outputs;
-    private Line inputsLine = null; 
-    private Line outputsLine = null; 
-    private Bounds bounds; //elem position in whiteBoard
+    private Line inputsLine = null;
+    private Line outputsLine = null;
+    private Bounds bounds; // elem position in whiteBoard
     private Orientation orientation;
     private int sizeMultiplicator = 1;
-    
-    
-    public GraphicElem(Elem elem)
-    {
+
+    public GraphicElem(Elem elem) {
         id = idCount++;
         this.elem = elem;
         attributes = GraphicElemInfo.getAttributes(elem);
         shape = GraphicElemInfo.getShape(elem);
-        inputs = new ArrayList<>(elem.getNbBusIn()); 
-        outputs = new ArrayList<>(elem.getNbBusOut());
-    }
-    
-    public GraphicElem(ElemLogique elem)
-    {
-        id = idCount++;
-        this.elem = elem;
-        attributes = GraphicElemInfo.getAttributes(elem);
-        shape = GraphicElemInfo.getShape(elem);
-        inputs = new ArrayList<>(elem.getNbBusIn()); 
+        inputs = new ArrayList<>(elem.getNbBusIn());
         outputs = new ArrayList<>(elem.getNbBusOut());
     }
 
-    public GraphicElem(ElemLogique elem,List<Object> attributes)
-    {
+    public GraphicElem(ElemLogique elem) {
+        id = idCount++;
+        this.elem = elem;
+        attributes = GraphicElemInfo.getAttributes(elem);
+        shape = GraphicElemInfo.getShape(elem);
+        inputs = new ArrayList<>(elem.getNbBusIn());
+        outputs = new ArrayList<>(elem.getNbBusOut());
+    }
+
+    public GraphicElem(ElemLogique elem, List<Object> attributes) {
         this(elem);
         this.attributes = new ArrayList<>(attributes.size());
     }
 
+    /**
+     * Returns the unique identifier of the graphic element.
+     *
+     * @return the unique identifier of the graphic element
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Returns the list of attributes associated with the GraphicElem instance.
+     *
+     * @return A list of attributes. Each attribute is represented as an Object.
+     *         The actual type of the attribute can be determined by checking the
+     *         attribute's name and value.
+     */
     public List<Object> getAttributes() {
         return attributes;
     }
 
     /*
-    public void setAttribute(Attribute<?> attri) 
-    {
-        for (Object elem : attributes) 
-        {
-            Attribute<?> attribute = (Attribute<?>)elem;
-            
-            if (attri.getName().equals(attribute.getName())) 
-                attribute.setValue(attri.getValue());
-        }
-    }
-    */
+     * public void setAttribute(Attribute<?> attri)
+     * {
+     * for (Object elem : attributes)
+     * {
+     * Attribute<?> attribute = (Attribute<?>)elem;
+     * 
+     * if (attri.getName().equals(attribute.getName()))
+     * attribute.setValue(attri.getValue());
+     * }
+     * }
+     */
 
-    public void setInOut()
-    {
+    public void setInOut() {
         setInOrOut(true);
         setInOrOut(false);
     }
 
-    private void setInOrOut(boolean setIn)
-    {
+    private void setInOrOut(boolean setIn) {
         int inOutNb = setIn ? elem.getNbBusIn() : elem.getNbBusOut();
         List<Circle> inOrOutInputs = setIn ? inputs : outputs;
         Line inOrOutInputLine = setIn ? inputsLine : outputsLine;
 
         Bounds elemBound = shape.getBoundsInParent();
-        double inputOccupiedSpace = 2*inputMargin + inputSpacing*(inOutNb-1) + inputRadius*2*inOutNb;
+        double inputOccupiedSpace = 2 * inputMargin + inputSpacing * (inOutNb - 1) + inputRadius * 2 * inOutNb;
 
-        Point2D.Double inputsStartingPos =  setIn ? new Point2D.Double(elemBound.getMinX()-inputMargin,elemBound.getMinY()) 
-                                                : new Point2D.Double(elemBound.getMaxX()+inputMargin,elemBound.getMinY()) ;
+        Point2D.Double inputsStartingPos = setIn
+                ? new Point2D.Double(elemBound.getMinX() - inputMargin, elemBound.getMinY())
+                : new Point2D.Double(elemBound.getMaxX() + inputMargin, elemBound.getMinY());
 
-        Point2D.Double inputsEndPos =  setIn ? new Point2D.Double(inputsStartingPos.getX(),elemBound.getMinY()+elemBound.getHeight())
-                                                : new Point2D.Double(inputsStartingPos.getX(),elemBound.getMinY()+elemBound.getHeight());
+        Point2D.Double inputsEndPos = setIn
+                ? new Point2D.Double(inputsStartingPos.getX(), elemBound.getMinY() + elemBound.getHeight())
+                : new Point2D.Double(inputsStartingPos.getX(), elemBound.getMinY() + elemBound.getHeight());
 
         double firstHalfEndingPos;
         double secondHalfEndingPos;
 
-        if(elemBound.getHeight() < inputOccupiedSpace )
-        {
-            double missingSpace = inputOccupiedSpace-elemBound.getHeight();
+        if (elemBound.getHeight() < inputOccupiedSpace) {
+            double missingSpace = inputOccupiedSpace - elemBound.getHeight();
 
-            inOrOutInputLine = new Line(inputsStartingPos.getX(),elemBound.getMinY()-missingSpace/2,inputsEndPos.getX(),inputsEndPos.getY()+missingSpace/2);
+            inOrOutInputLine = new Line(inputsStartingPos.getX(), elemBound.getMinY() - missingSpace / 2,
+                    inputsEndPos.getX(), inputsEndPos.getY() + missingSpace / 2);
 
-            inputsStartingPos.setLocation(inputsStartingPos.getX(),elemBound.getMinY()-missingSpace/2);
-            inputsEndPos.setLocation(inputsStartingPos.getX(), inputsEndPos.getY()+missingSpace/2);
+            inputsStartingPos.setLocation(inputsStartingPos.getX(), elemBound.getMinY() - missingSpace / 2);
+            inputsEndPos.setLocation(inputsStartingPos.getX(), inputsEndPos.getY() + missingSpace / 2);
         }
 
         double currStartingPos = inputsStartingPos.getY();
 
-        for(int inputNb = 0; inputNb < inOutNb/2; ++inputNb)
-        {
-            inOrOutInputs.add(new Circle(inputsStartingPos.getX()-inputRadius,currStartingPos+inputRadius,inputRadius));
-            currStartingPos+=2*inputRadius+inputSpacing;
+        for (int inputNb = 0; inputNb < inOutNb / 2; ++inputNb) {
+            inOrOutInputs.add(
+                    new Circle(inputsStartingPos.getX() - inputRadius, currStartingPos + inputRadius, inputRadius));
+            currStartingPos += 2 * inputRadius + inputSpacing;
         }
-        firstHalfEndingPos = currStartingPos-inputSpacing;
+        firstHalfEndingPos = currStartingPos - inputSpacing;
 
         currStartingPos = inputsEndPos.getY();
-        for(int inputNb = 0; inputNb < inOutNb/2; ++inputNb)
-        {
-            inOrOutInputs.add(new Circle(inputsStartingPos.getX()-inputRadius,currStartingPos-inputRadius,inputRadius));
-            currStartingPos-=2*inputRadius+inputSpacing;
+        for (int inputNb = 0; inputNb < inOutNb / 2; ++inputNb) {
+            inOrOutInputs.add(
+                    new Circle(inputsStartingPos.getX() - inputRadius, currStartingPos - inputRadius, inputRadius));
+            currStartingPos -= 2 * inputRadius + inputSpacing;
         }
-        secondHalfEndingPos = currStartingPos-inputSpacing;
+        secondHalfEndingPos = currStartingPos - inputSpacing;
 
-        if (inOutNb%2!=0)
-        {
-            double halfWayPos = inputsStartingPos.getY() + Math.abs((firstHalfEndingPos - secondHalfEndingPos) /2);
-            inOrOutInputs.add(new Circle(inputsStartingPos.getX()-inputRadius,halfWayPos,inputRadius));
-        } 
+        if (inOutNb % 2 != 0) {
+            double halfWayPos = inputsStartingPos.getY() + Math.abs((firstHalfEndingPos - secondHalfEndingPos) / 2);
+            inOrOutInputs.add(new Circle(inputsStartingPos.getX() - inputRadius, halfWayPos, inputRadius));
+        }
 
-        for(Circle circle : inOrOutInputs)
-        {
+        for (Circle circle : inOrOutInputs) {
             circle.getStyleClass().add("IOCircles");
         }
     }
 
-    public Shape getShape() 
-    {
+    /**
+     * Returns the graphical representation of the element associated with this
+     * GraphicElem instance.
+     *
+     * @return The Shape object representing the graphical element.
+     *         The Shape object can be of any type, such as Rectangle, Circle, Line,
+     *         or a custom Shape.
+     *         The actual type of the Shape object can be determined by checking the
+     *         type of the Shape class.
+     */
+    public Shape getShape() {
         return shape;
     }
-    
-    public void setShape(Shape shape)
-    {
+
+    /**
+     * Sets the graphical representation of the element associated with this
+     * GraphicElem instance.
+     *
+     * @param shape The Shape object representing the graphical element.
+     *
+     */
+    public void setShape(Shape shape) {
         this.shape = shape;
     }
 
@@ -167,59 +183,182 @@ public class GraphicElem
         return elem;
     }
 
-    public Object clone()
-    {
+    public Object clone() {
         return null;
     }
 
-    public Bounds getBounds() 
-    {
+    /**
+     * Returns the bounding box of the graphical representation of the element.
+     *
+     * @return The Bounds object representing the bounding box of the graphical
+     *         element.
+     *         The Bounds object contains the minimum and maximum x and y
+     *         coordinates of the element's shape.
+     *         This information can be used to position and manipulate the graphical
+     *         element within a graphical user interface (GUI).
+     *         The actual type of the Bounds object can be determined by checking
+     *         the type of the Bounds class.
+     */
+    public Bounds getBounds() {
         return bounds;
     }
 
-    public void setBounds(Bounds bounds) 
-    {
+    /**
+     * Sets the bounding box of the graphical representation of the element.
+     *
+     * @param bounds The Bounds object representing the bounding box of the
+     *               graphical element.
+     *               The Bounds object contains the minimum and maximum x and y
+     *               coordinates of the element's shape.
+     *               This information can be used to position and manipulate the
+     *               graphical element within a graphical user interface (GUI).
+     *               The actual type of the Bounds object can be determined by
+     *               checking the type of the Bounds class.
+     */
+    public void setBounds(Bounds bounds) {
         this.bounds = bounds;
     }
 
-    public Orientation getOrientation() 
-    {
+    /**
+     * Returns the orientation of the graphical element.
+     *
+     * @return The orientation of the graphical element.
+     *         The orientation can be one of the following: EAST, SOUTH, WEST, or
+     *         NORTH.
+     *         This information can be used to determine the direction in which the
+     *         graphical element is facing within a graphical user interface (GUI).
+     */
+    public Orientation getOrientation() {
         return orientation;
     }
 
-    public void setOrientation(Orientation orient) 
-    {
+    /**
+     * Sets the orientation of the graphical element.
+     *
+     * @param orient The orientation to be set for the graphical element.
+     *               The orientation can be one of the following: EAST, SOUTH, WEST,
+     *               or NORTH.
+     *               This information can be used to determine the direction in
+     *               which the graphical element is facing within a graphical user
+     *               interface (GUI).
+     *               The provided orientation value will be assigned to the
+     *               'orientation' attribute of the GraphicElem instance.
+     *
+     * @return void
+     *         This method does not return any value. It only sets the orientation
+     *         attribute of the GraphicElem instance.
+     */
+    public void setOrientation(Orientation orient) {
         orientation = orient;
     }
 
-    public List<Circle> getInputs() 
-    {
+    /**
+     * Returns the list of input circles associated with the graphical element.
+     * Each input circle represents a connection point for incoming connections.
+     *
+     * @return A list of Circle objects representing the input circles.
+     *         The Circle objects can be used to visualize and interact with the
+     *         input connections.
+     *         The actual type of the Circle object can be determined by checking
+     *         the type of the Circle class.
+     *         The list is empty if there are no input connections for the graphical
+     *         element.
+     */
+    public List<Circle> getInputs() {
         return inputs;
     }
 
-    public List<Circle> getOutputs()
-    {
+    /**
+     * Returns the list of output circles associated with the graphical element.
+     * Each output circle represents a connection point for outgoing connections.
+     *
+     * @return A list of Circle objects representing the output circles.
+     *         The Circle objects can be used to visualize and interact with the
+     *         output connections.
+     *         The actual type of the Circle object can be determined by checking
+     *         the type of the Circle class.
+     *         The list is empty if there are no output connections for the
+     *         graphical element.
+     */
+    public List<Circle> getOutputs() {
         return outputs;
     }
 
-    public Line getInputsLine() 
-    {
+    /**
+     * Returns the line representing the input connections for the graphical
+     * element.
+     * This line is used to visually connect the input circles to the graphical
+     * element.
+     *
+     * @return A Line object representing the input connections.
+     *         The Line object connects the input circles to the graphical element.
+     *         The actual type of the Line object can be determined by checking the
+     *         type of the Line class.
+     *         The Line object is null if there are no input connections for the
+     *         graphical element.
+     */
+    public Line getInputsLine() {
         return inputsLine;
     }
 
-    public Line getOutputsLine()
-    {
+    /**
+     * Returns the line representing the output connections for the graphical
+     * element.
+     * This line is used to visually connect the output circles to the graphical
+     * element.
+     *
+     * @return A Line object representing the output connections.
+     *         The Line object connects the output circles to the graphical element.
+     *         The actual type of the Line object can be determined by checking the
+     *         type of the Line class.
+     *         The Line object is null if there are no output connections for the
+     *         graphical element.
+     */
+    public Line getOutputsLine() {
         return outputsLine;
 
     }
 
-    public int getsizeMultiplicator()
-    {
+    /**
+     * Returns the size multiplicator of the graphical element.
+     * The size multiplicator is used to scale the graphical representation of the
+     * element.
+     *
+     * @return An integer representing the size multiplicator.
+     *         The size multiplicator is a positive integer value that determines
+     *         the scaling factor of the graphical element.
+     *         A value of 1 indicates that the element is displayed at its original
+     *         size.
+     *         A value greater than 1 indicates that the element is scaled up by the
+     *         specified factor.
+     *         A value less than 1 indicates that the element is scaled down by the
+     *         specified factor.
+     */
+    public int getsizeMultiplicator() {
         return sizeMultiplicator;
     }
 
-    public void set(int sizeMultiplicator)
-    {
+    /**
+     * Sets the size multiplicator of the graphical element.
+     * The size multiplicator is used to scale the graphical representation of the
+     * element.
+     *
+     * @param sizeMultiplicator An integer representing the size multiplicator.
+     *                          The size multiplicator is a positive integer value
+     *                          that determines the scaling factor of the graphical
+     *                          element.
+     *                          A value of 1 indicates that the element is displayed
+     *                          at its original size.
+     *                          A value greater than 1 indicates that the element is
+     *                          scaled up by the specified factor.
+     *                          A value less than 1 indicates that the element is
+     *                          scaled down by the specified factor.
+     *
+     * @return void
+     *         This method does not return any value. It only sets the size
+     *         multiplicator attribute of the GraphicElem instance.
+     */
+    public void set(int sizeMultiplicator) {
         this.sizeMultiplicator = sizeMultiplicator;
     }
 }
